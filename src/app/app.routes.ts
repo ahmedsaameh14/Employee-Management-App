5ba1d46bc8ce071c4f27f61b307b2c64ee87e98d
Ahmed_Sameh <ahmedsaameh14@gmail.com>
2025-10-22 17:12:39 +0300

    Add Navbar with RouterLink , Add Routes and set Not Found Page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { EmployeeListComponent } from './Pages/employee-list/employee-list.component';
+import { EmployeeFormComponent } from './Pages/employee-form/employee-form.component';
+import { NotfoundComponent } from './Pages/notfound/notfound.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path:'' , redirectTo:'list' , pathMatch:'full' },
+    { path:'list' , component:EmployeeListComponent , title:'Emp List' },
+    { path:'new-employee' , component:EmployeeFormComponent },
+    { path:'**' , component:NotfoundComponent }
+];
